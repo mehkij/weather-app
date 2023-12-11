ac19015deb52ac3adefa81570259286058808985
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-12-11 12:19:35 -0500

    Allow for user input to interact with getCurrentWeather
    The searchbar integrates the auto-complete API from Weather API to get the accurate location and weather data to be used in getCurrentWeather

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import { getCurrentWeather } from "./weather";
+
 const searchbar = document.querySelector(".searchbar");
 const submit = document.querySelector(".submit");
 
+// When the user presses the "submit" button, we send the value of the search bar to the Weather API's auto-complete API to get the correct location data
 submit.addEventListener("click", function(e) {
   fetch("https://api.weatherapi.com/v1/search.json?key=857d876ab01b4f8a8b242735231112&q=" + searchbar.value, {mode: "cors"})
     .then(function(response) {
@@ -8,7 +11,7 @@ submit.addEventListener("click", function(e) {
     })
     .then(function(response) {
       let locationData = response[0].url;
-      return locationData;
+      getCurrentWeather(locationData);
     })
     .catch(function(error) {
       console.error("Error fetching data:", error);
