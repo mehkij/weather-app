4ab6df55843e7a72f0c12e3f90c2a16f71550a39
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-12-11 14:33:33 -0500

    Remove submit button and its event listener

--- src/index.js
+++ src/index.js
@@ -2,25 +2,8 @@ import { getCurrentWeather, getLocation } from "./weather";
 import "./style.css";
 
 const searchbar = document.querySelector(".searchbar");
-const submit = document.querySelector(".submit");
 
-// When the user presses the "submit" button, we send the value of the search bar to the Weather API's auto-complete API to get the correct location data
-submit.addEventListener("click", function(e) {
-  fetch("https://api.weatherapi.com/v1/search.json?key=857d876ab01b4f8a8b242735231112&q=" + searchbar.value, {mode: "cors"})
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      let locationData = response[0].url;
-      getCurrentWeather(locationData);
-      getLocation(locationData);
-      searchbar.value = "";
-    })
-    .catch(function(error) {
-      console.error("Error fetching location data:", error);
-    });
-});
-
+// When the user presses the "enter" key, we send the value of the search bar to the Weather API's auto-complete API to get the correct location data
 searchbar.addEventListener("keydown", function(e) {
   if (e.code == "Enter") {
     fetch("https://api.weatherapi.com/v1/search.json?key=857d876ab01b4f8a8b242735231112&q=" + searchbar.value, {mode: "cors"})
