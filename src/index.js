0322023c988eb697be6dc0bd9884c9611b54a93c
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-12-11 00:32:32 -0500

    Add searchbar functionality
    Allow the user to input a location in the searchbar, and return an array of location data using the Weather API

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const searchbar = document.querySelector(".searchbar");
+const submit = document.querySelector(".submit");
+
+submit.addEventListener("click", function(e) {
+  fetch("https://api.weatherapi.com/v1/search.json?key=857d876ab01b4f8a8b242735231112&q=" + searchbar.value, {mode: "cors"})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.error("Error fetching data:", error);
+    });
+});
